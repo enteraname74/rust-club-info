ef2c1f184ccd10c01cfdf94ba48635ad25b2dc9b
enteraname74 <noahpenin@gmail.com>
2023-11-22 16:58:49 +0100

    Fin séance 2

--- seance_2/11-error_handling.rs
+++ seance_2/11-error_handling.rs
@@ -17,31 +17,31 @@ fn main() {
 }
 
 // Même chose qu'en haut, mais en retournant un message spécifique au type d'erreur.
-fn main() {
-    let greeting_file_result = File::open("hello.txt");
+// fn main() {
+//     let greeting_file_result = File::open("hello.txt");
 
-    let greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file: {:?}", e),
-            },
-            other_error => {
-                panic!("Problem opening the file: {:?}", other_error);
-            }
-        },
-    };
-}
+//     let greeting_file = match greeting_file_result {
+//         Ok(file) => file,
+//         Err(error) => match error.kind() {
+//             ErrorKind::NotFound => match File::create("hello.txt") {
+//                 Ok(fc) => fc,
+//                 Err(e) => panic!("Problem creating the file: {:?}", e),
+//             },
+//             other_error => {
+//                 panic!("Problem opening the file: {:?}", other_error);
+//             }
+//         },
+//     };
+// }
 
-fn main() {
-    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
-        if error.kind() == ErrorKind::NotFound {
-            File::create("hello.txt").unwrap_or_else(|error| {
-                panic!("Problem creating the file: {:?}", error);
-            })
-        } else {
-            panic!("Problem opening the file: {:?}", error);
-        }
-    });
-}
+// fn main() {
+//     let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+//         if error.kind() == ErrorKind::NotFound {
+//             File::create("hello.txt").unwrap_or_else(|error| {
+//                 panic!("Problem creating the file: {:?}", error);
+//             })
+//         } else {
+//             panic!("Problem opening the file: {:?}", error);
+//         }
+//     });
+// }
